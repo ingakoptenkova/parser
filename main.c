a185d2810970412c3b12f3abe12c78a3ca886211
Alexey Koptenkov <alexey.koptenkov@yandex.ru>
2015-12-10 20:21:12 +0200

    repackaging

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main() {
     char token[20];
 
-    FILE *fp = fopen("c:\\users\\alexey\\git\\lexica\\example.txt", "r");
+    FILE *fp = fopen("c:\\users\\inga\\git\\lexica\\example.txt", "r");
     if (fp == NULL) {
         fprintf(stderr, "Error in opening file\n");
         return EXIT_FAILURE;
